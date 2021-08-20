58b0ee9fec2ec01717a364bfae7e9895386c235e
juliantrovato <julian_todobien@hotmail.com>
2021-08-20 17:27:05 -0300

    first commit

---
+++ server/rtdb.ts
@@ -0,0 +1,13 @@
+import *as firebaseAdmin from "firebase-admin";
+import *as serviceAccount from "./key.json";
+
+
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount as any),
+  databaseURL: "https://modulo-6-default-rtdb.firebaseio.com"
+});
+
+const fs = firebaseAdmin.firestore();
+const rtdb = firebaseAdmin.database();
+export {fs, rtdb};
